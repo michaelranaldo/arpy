0363da25fb1530ce26712b975cd4285f4b052c5c
Michael Ranaldo <michael@ranaldo.co.uk>
2019-11-03 01:57:17 +0000

    Basically 90% done. Quite operational for if your friends arrive

--- arpy.py
+++ arpy.py
@@ -1,20 +1,48 @@
 #!/usr/bin/python3
 
 from scapy.all import *
-import time
+import time, ipaddress
 
+verbose = False
 found_hosts={}
 
 def arp_monitor_callback(packet):
     if ARP in packet and packet[ARP].op in (1,2):
-        print(packet.sprintf("%ARP.hwsrc%"))
-        found_hosts[packet.sprintf("%ARP.hwsrc%")] = packet.sprintf("%ARP.psrc%")
-        return packet.sprintf("Found %ARP.hwsrc% %ARP.psrc%")
+        if not (packet.sprintf("%ARP.hwsrc%") in found_hosts):
+            found_hosts[packet.sprintf("%ARP.hwsrc%")] = packet.sprintf("%ARP.psrc%")
+            return packet.sprintf("Found New %ARP.hwsrc% %ARP.psrc%")
+        else:
+            if verbose:
+                print("Ignoring previously discovered host")
+
+def determine_subnet_cidr(hosts):
+    subnet_range = hosts[len(hosts) - 1] - hosts[0] + 1
+    print("Subnet range is %s " % subnet_range)
+    print("Determining subnets...")
+    print("The captured ip addresses range from %s to %s" % (ipaddress.IPv4Address(hosts[0]), ipaddress.IPv4Address(hosts[len(hosts) - 1])))
+    print("Based on a sample of %s captured ip addresses" % len(hosts))
+    # powers of 2 til we get to something
+    exponent = 0
+    estimated_subnet_size = 1
+    cidr = 32
+    chk = True
+    while chk:
+        if (2 ** exponent) < subnet_range:
+            estimated_subnet_size = 2 ** exponent
+            exponent = exponent + 1
+            cidr = cidr - 1
+        else:
+            chk = False
+
+    print("Estimated subnet size: /%s" % cidr)
+    return cidr
+
 
 # determine which potential subnets IP address could be in
 ## this will likely tie in with the below
 ## i.e. if only 255 addresses have been found, perhaps this is on balance
 ## a /24, therefore suggest quiet sports within the /24
+
 
 # determine which IP addresses have been discovered so far
 
@@ -30,4 +58,24 @@ def arp_monitor_callback(packet):
 
 sniff(prn=arp_monitor_callback, filter="arp", store=0)
 print()
-print(found_hosts)
+
+#sort ip addresses
+#get highest and lowest
+#ip address library allows for addr in subn and addr < addr operations
+
+numeric_hosts=[]
+for host in found_hosts:
+    numeric_hosts.append(int(ipaddress.IPv4Address(found_hosts[host])))
+numeric_hosts.sort()
+
+network_address=str(ipaddress.IPv4Address(numeric_hosts[0]))+"/"+str(determine_subnet_cidr(numeric_hosts))
+print(network_address)
+network = ipaddress.ip_network(network_address, strict=False)
+print("Network address: %s" % network.network_address)
+print("Broadcast address: %s" % network.broadcast_address)
+if network.is_private:
+    print("The network appears to be within a private address range")
+
+#for host in found_hosts:
+#    print("Found %s" % host)
+#    print("with an IP of %s" % found_hosts[host])
