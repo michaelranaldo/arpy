d9a791642a0cab3540569e3a47a0d9c3bbec37b0
Michael Ranaldo <michael@ranaldo.co.uk>
2019-11-04 16:05:30 +0000

    Added verbose mode

--- arpy.py
+++ arpy.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 
 from scapy.all import *
-import time, ipaddress
+import time, ipaddress, argparse
 
-verbose = False
 found_hosts={}
 ignore = ['0.0.0.0']
+
+parser = argparse.ArgumentParser(description = "ARP listener")
+parser.add_argument('-v', '--verbose', help='Verbose mode', action='count')
+args = parser.parse_args()
+
+if args.verbose != None and int(args.verbose) > 0:
+    verbose = True
+else:
+    verbose = False
 
 def arp_monitor_callback(packet):
     if ARP in packet and packet[ARP].op in (1,2):
@@ -19,10 +27,11 @@ def arp_monitor_callback(packet):
 def determine_subnet_cidr(hosts):
     subnet_range = hosts[len(hosts) - 1] - hosts[0]
     final_host = ipaddress.IPv4Address(hosts[len(hosts) - 1])
-    print("Subnet range is %s " % subnet_range)
-    print("Determining subnets...")
-    print("The captured ip addresses range from %s to %s" % (ipaddress.IPv4Address(hosts[0]), final_host))
-    print("Based on a sample of %s captured ip addresses" % len(hosts))
+    if verbose:
+        print("Subnet range is %s " % subnet_range)
+        print("Determining subnets...")
+        print("The captured ip addresses range from %s to %s" % (ipaddress.IPv4Address(hosts[0]), final_host))
+        print("Based on a sample of %s captured ip addresses" % len(hosts))
     # powers of 2 til we get to something
     exponent = 0
     estimated_subnet_size = 1
@@ -33,7 +42,6 @@ def determine_subnet_cidr(hosts):
         # and if the maximum proposed range is still 
         # lower than the highest ip address
         network = ipaddress.ip_network(str(ipaddress.IPv4Address(numeric_hosts[0]))+"/"+str(cidr), strict=False)
-        print(network.broadcast_address)
         if (2 ** exponent) < subnet_range or final_host not in network:
             estimated_subnet_size = 2 ** exponent
             exponent = exponent + 1
@@ -41,11 +49,13 @@ def determine_subnet_cidr(hosts):
         else:
             chk = False
 
-    print("Estimated subnet size: /%s" % cidr)
+    if verbose:
+        print("Estimated subnet size: /%s" % cidr)
     return cidr
 
 def get_potential_addresses(found_hosts, network):
-    print("Getting potential addresses")
+    if verbose:
+        print("Getting potential addresses...")
     empty_slots = []
     # Roll through the network and identify the first x empty slots
     for address in ipaddress.IPv4Network(network):
@@ -83,7 +93,8 @@ try :
     sniffo.stop()
 except KeyboardInterrupt:
     print()
-    print("Well fuck you too buddy")
+    if verbose:
+        print("Well fuck you too buddy")
     sniffo.stop()
 print()
 if len(found_hosts) == 0:
@@ -97,8 +108,9 @@ numeric_hosts.sort()
 cidr = determine_subnet_cidr(numeric_hosts)
 full_network_address = str(ipaddress.IPv4Address(numeric_hosts[0]))+"/"+str(cidr)
 network = ipaddress.ip_network(full_network_address, strict=False)
-print("The network is within a %s address range" % ("private" if network.is_private else "public"))
-print("Network address: %s" % network)
+if verbose:
+    print("The network is within a %s address range" % ("private" if network.is_private else "public"))
+print("Suspected subnet: %s" % network)
 empty_addresses = ( 2 ** (32 - cidr) ) - len(found_hosts)
 
 if empty_addresses > 0:
